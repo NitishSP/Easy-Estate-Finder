578b3bddf4ac21ad8a16e6d34ebcd7d806b192fb
Nitish Singh <nitishsinghshreenet@gmail.com>
2024-08-07 12:17:30 +0530

    Update apiRequest.js

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "https://easy-estate-finder-backend.onrender.com/api/",
+    baseURL: "https://easy-estate-finder-backend.onrender.com/api",
     withCredentials: true,
 });
 
